dfeec66ef6af4004c1b1ada610d8448f4bd59865
Martin Baulig <martin.baulig@googlemail.com>
2012-06-09 00:11:04 +0200

    SqlCommand cancellation tests.
    These work on Windows, but currently fail with Mono.

--- TestSqlClient/AssertEx.cs
+++ TestSqlClient/AssertEx.cs
@@ -5,33 +5,30 @@ namespace TestSqlClient
 {
 	public static class AssertEx
 	{
+		public static void Exception<T> (Action action)
+			where T:Exception
+		{
+			Exception<T> (action, null);
+		}
+
 		public static void Exception<T> (Action action, string message)
 			where T:Exception
 		{
+			var prefix = message != null ? message + ": " : "";
 			try {
 				action ();
-				Assert.Fail (message);
+				Assert.Fail (string.Format (
+					"{0}Expected exception of type {1}", prefix, typeof (T)));
 			} catch (Exception ex) {
+				if (ex is AssertionException)
+					throw;
 				if (ex is AggregateException)
 					ex = ((AggregateException) ex).InnerException;
-				Assert.IsInstanceOfType (typeof (T), ex, message);
-			}
-		}
-
-		public static void Exception (Type t, Action action, string message)
-		{
-			Exception (t, action, message);
-		}
-
-		public static void Exception (Type t, Action action, string message, bool allowAggregate)
-		{
-			try {
-				action ();
-				Assert.Fail (message);
-			} catch (Exception ex) {
-				if (allowAggregate && (ex is AggregateException))
-					ex = ((AggregateException) ex).InnerException;
-				Assert.IsInstanceOfType (t, ex, message);
+				if (ex is T)
+					return;
+				Assert.Fail (string.Format (
+					"{0}Expected exception of type {1}, but got {2}",
+					prefix, typeof (T), ex));
 			}
 		}
 	}
--- TestSqlClient/TestDatabase.cs
+++ TestSqlClient/TestDatabase.cs
@@ -22,14 +22,37 @@ namespace TestSqlClient
 			private set;
 		}
 
-		[TestFixtureSetUp]
+		public static TimeSpan NetworkTimeout {
+			get {
+				if (Environment.OSVersion.Platform == PlatformID.Win32NT) {
+					// We're on the same machine.
+					return TimeSpan.FromMilliseconds (250);
+				} else {
+					// We're using a network connection.
+					return TimeSpan.FromMilliseconds (2500);
+				}
+			}
+		}
+
+		public static TimeSpan LongTimeout {
+			get {
+				if (Environment.OSVersion.Platform == PlatformID.Win32NT) {
+					// Must be more than one second.
+					return TimeSpan.FromMilliseconds (1500);
+				} else {
+					return TimeSpan.FromMilliseconds (6000);
+				}
+			}
+		}
+
+		[SetUp]
 		public void SetUp ()
 		{
 			Connection = new SqlConnection (ConnectionString);
-			Assert.That (Connection.OpenAsync ().Wait (2500));
+			Assert.That (Connection.OpenAsync ().Wait (NetworkTimeout));
 		}
 
-		[TestFixtureTearDown]
+		[TearDown]
 		public void TearDown ()
 		{
 			Connection.Close ();
@@ -42,11 +65,11 @@ namespace TestSqlClient
 
 			var create = new SqlCommand (
 				string.Format ("CREATE TABLE {0} (a int)", tmpname), Connection);
-			Assert.That (create.ExecuteNonQueryAsync ().Wait (1000));
+			Assert.That (create.ExecuteNonQueryAsync ().Wait (NetworkTimeout));
 
 			var drop = new SqlCommand (
 				string.Format ("DROP TABLE {0}", tmpname), Connection);
-			Assert.That (drop.ExecuteNonQueryAsync ().Wait (1000));
+			Assert.That (drop.ExecuteNonQueryAsync ().Wait (NetworkTimeout));
 		}
 
 		[Test]
@@ -54,10 +77,57 @@ namespace TestSqlClient
 		{
 			var cmd = new SqlCommand ("SELECT * FROM test", Connection);
 			var task = cmd.ExecuteReaderAsync ();
-			Assert.That (task.Wait (2500));
+			Assert.That (task.Wait (NetworkTimeout));
 
 			var reader = task.Result;
 			Assert.That (reader.FieldCount > 0);
+			reader.Close ();
 		}
+
+		[Test]
+		public void MustCloseReader ()
+		{
+			var cmd = new SqlCommand ("SELECT * FROM test", Connection);
+			Console.WriteLine ("TEST: {0}", cmd.CommandTimeout);
+			var task = cmd.ExecuteReaderAsync ();
+			Assert.That (task.Wait (NetworkTimeout));
+
+			try {
+				var errorCmd = new SqlCommand ("SELECT * FROM test", Connection);
+				AssertEx.Exception<InvalidOperationException> (
+					() => errorCmd.ExecuteReaderAsync ().Wait (NetworkTimeout));
+			} finally {
+				task.Result.Close ();
+			}
+		}
+
+		[Test]
+		public void TestCommandTimeout ()
+		{
+			var cmd = new SqlCommand ("WAITFOR DELAY '00:00:15'", Connection);
+			cmd.CommandTimeout = (int)(NetworkTimeout.TotalMilliseconds + 999) / 1000;
+			var task = cmd.ExecuteNonQueryAsync ();
+			AssertEx.Exception<SqlException> (() => task.Wait (LongTimeout));
+		}
+
+		[Test]
+		public void Cancel ()
+		{
+			var cmd = new SqlCommand ("WAITFOR DELAY '00:00:15'", Connection);
+			var task = cmd.ExecuteNonQueryAsync ();
+			Task.Delay (NetworkTimeout).Wait ();
+			cmd.Cancel ();
+			AssertEx.Exception<SqlException> (() => task.Wait (LongTimeout));
+		}
+
+		[Test]
+		public void CancelWithToken ()
+		{
+			var cmd = new SqlCommand ("WAITFOR DELAY '00:00:15'", Connection);
+			var cts = new CancellationTokenSource (NetworkTimeout);
+			var task = cmd.ExecuteNonQueryAsync (cts.Token);
+			AssertEx.Exception<SqlException> (() => task.Wait (LongTimeout));
+		}
+
 	}
 }
