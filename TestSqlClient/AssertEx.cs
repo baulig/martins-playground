f9d55ccea53c7c646927c1beffadf9251c0b3c15
Martin Baulig <martin.baulig@googlemail.com>
2012-06-11 03:46:40 +0200

    Updated tests.

--- TestSqlClient/AssertEx.cs
+++ TestSqlClient/AssertEx.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace TestSqlClient
@@ -19,9 +21,9 @@ namespace TestSqlClient
 				action ();
 				Assert.Fail (string.Format (
 					"{0}Expected exception of type {1}", prefix, typeof (T)));
+			} catch (AssertionException) {
+				throw;
 			} catch (Exception ex) {
-				if (ex is AssertionException)
-					throw;
 				if (ex is AggregateException)
 					ex = ((AggregateException) ex).InnerException;
 				if (ex is T)
@@ -31,5 +33,106 @@ namespace TestSqlClient
 					prefix, typeof (T), ex));
 			}
 		}
+
+		public static void TaskCompleted (Task task, int timeout)
+		{
+			TaskCompleted (task, timeout, null);
+		}
+
+		public static void TaskCompleted (Task task, int timeout, string message)
+		{
+			TaskCompleted (task, TimeSpan.FromMilliseconds (timeout), message);
+		}
+
+		public static void TaskCompleted (Task task, TimeSpan timeout)
+		{
+			TaskCompleted (task, timeout, null);
+		}
+
+		public static void TaskCompleted (Task task, TimeSpan timeout, string message)
+		{
+			var suffix = message != null ? ": " + message : "";
+			try {
+				if (!task.Wait (timeout))
+					Assert.Fail ("Task timed-out" + suffix);
+			} catch (OperationCanceledException) {
+				Assert.Fail ("Task canceled" + suffix);
+			} catch (AssertionException) {
+				throw;
+			} catch (Exception ex) {
+				Assert.Fail (string.Format ("Task threw exception '{0}'{1}", ex, suffix));
+			}
+		}
+
+		public static TResult TaskCompleted<TResult> (Task<TResult> task, int timeout)
+		{
+			return TaskCompleted (task, timeout, null);
+		}
+
+		public static TResult TaskCompleted<TResult> (Task<TResult> task, int timeout, string message)
+		{
+			return TaskCompleted (task, TimeSpan.FromMilliseconds (timeout), message);
+		}
+
+		public static TResult TaskCompleted<TResult> (Task<TResult> task, TimeSpan timeout)
+		{
+			return TaskCompleted (task, timeout, null);
+		}
+
+		public static TResult TaskCompleted<TResult> (Task<TResult> task, TimeSpan timeout, string message)
+		{
+			var suffix = message != null ? ": " + message : "";
+			try {
+				if (!task.Wait (timeout))
+					Assert.Fail ("Task timed-out" + suffix);
+				return task.Result;
+			} catch (OperationCanceledException) {
+				Assert.Fail ("Task canceled" + suffix);
+				throw;
+			} catch (Exception ex) {
+				Assert.Fail (string.Format ("Task threw exception '{0}'{1}", ex, suffix));
+				throw;
+			}
+		}
+
+		public static void TaskFailed<T> (Task task, int timeout)
+		{
+			TaskFailed<T> (task, timeout, null);
+		}
+
+		public static void TaskFailed<T> (Task task, int timeout, string message)
+		{
+			TaskFailed<T> (task, TimeSpan.FromMilliseconds (timeout), message);
+		}
+
+		public static void TaskFailed<T> (Task task, TimeSpan timeout)
+		{
+			TaskFailed<T> (task, timeout, null);
+		}
+
+		public static void TaskFailed<T> (Task task, TimeSpan timeout, string message)
+		{
+			var suffix = message != null ? ": " + message : "";
+			try {
+				if (!task.Wait (timeout))
+					Assert.Fail ("Task timed-out" + suffix);
+				Assert.Fail (string.Format (
+					"Expected exception of type {0}{1}", typeof (T), suffix));
+			} catch (OperationCanceledException) {
+				Assert.Fail ("Task canceled" + suffix);
+				throw;
+			} catch (AssertionException) {
+				throw;
+			} catch (Exception ex) {
+				if (ex is AggregateException)
+					ex = ((AggregateException) ex).InnerException;
+				if (ex is T)
+					return;
+				Assert.Fail (string.Format (
+					"Expected exception of type {0}, but got {1}{2}",
+					typeof (T), ex, suffix));
+			}
+		}
+
 	}
 }
--- TestSqlClient/TestDatabase.cs
+++ TestSqlClient/TestDatabase.cs
@@ -26,7 +26,7 @@ namespace TestSqlClient
 			get {
 				if (Environment.OSVersion.Platform == PlatformID.Win32NT) {
 					// We're on the same machine.
-					return TimeSpan.FromMilliseconds (250);
+					return TimeSpan.FromMilliseconds (750);
 				} else {
 					// We're using a network connection.
 					return TimeSpan.FromMilliseconds (2500);
@@ -38,10 +38,16 @@ namespace TestSqlClient
 			get {
 				if (Environment.OSVersion.Platform == PlatformID.Win32NT) {
 					// Must be more than one second.
-					return TimeSpan.FromMilliseconds (1500);
+					return TimeSpan.FromMilliseconds (2500);
 				} else {
 					return TimeSpan.FromMilliseconds (6000);
 				}
+			}
+		}
+
+		public int CommandTimeout {
+			get {
+				return (int)(NetworkTimeout.TotalMilliseconds + 999) / 1000;
 			}
 		}
 
@@ -49,7 +55,8 @@ namespace TestSqlClient
 		public void SetUp ()
 		{
 			Connection = new SqlConnection (ConnectionString);
-			Assert.That (Connection.OpenAsync ().Wait (NetworkTimeout));
+			Assert.That (Connection.OpenAsync ().Wait (NetworkTimeout),
+			             "Database connection failed.");
 		}
 
 		[TearDown]
@@ -58,33 +65,42 @@ namespace TestSqlClient
 			Connection.Close ();
 		}
 
+		SqlCommand CreateCommand (string text)
+		{
+			var cmd = new SqlCommand (text, Connection);
+			cmd.CommandTimeout = CommandTimeout;
+			return cmd;
+		}
+
+		SqlCommand CreateCommand (string text, params object[] args)
+		{
+			return CreateCommand (string.Format (text, args));
+		}
+
 		[Test]
 		public void CreateTable ()
 		{
 			var tmpname = string.Format ("tmp{0}", DateTime.Now.Ticks);
 
-			var create = new SqlCommand (
-				string.Format ("CREATE TABLE {0} (a int)", tmpname), Connection);
-			Assert.That (create.ExecuteNonQueryAsync ().Wait (NetworkTimeout));
+			var create = CreateCommand ("CREATE TABLE {0} (a int)", tmpname);
+			AssertEx.TaskCompleted (create.ExecuteNonQueryAsync (), NetworkTimeout);
 
-			var drop = new SqlCommand (
-				string.Format ("DROP TABLE {0}", tmpname), Connection);
-			Assert.That (drop.ExecuteNonQueryAsync ().Wait (NetworkTimeout));
+			var drop = CreateCommand ("DROP TABLE {0}", tmpname);
+			AssertEx.TaskCompleted (drop.ExecuteNonQueryAsync (), NetworkTimeout);
 		}
 
 		[Test]
+		[Category("Database")]
 		public void ReadTest ()
 		{
-			var cmd = new SqlCommand ("SELECT * FROM test", Connection);
-			var task = cmd.ExecuteReaderAsync ();
-			Assert.That (task.Wait (NetworkTimeout));
-
-			var reader = task.Result;
+			var cmd = CreateCommand ("SELECT * FROM test");
+			var reader = AssertEx.TaskCompleted (cmd.ExecuteReaderAsync (), NetworkTimeout);
 			Assert.That (reader.FieldCount > 0);
 			reader.Close ();
 		}
 
 		[Test]
+		[Category("NotWorking")]
 		public void MustCloseReader ()
 		{
 			var cmd = new SqlCommand ("SELECT * FROM test", Connection);
@@ -93,41 +109,169 @@ namespace TestSqlClient
 			Assert.That (task.Wait (NetworkTimeout));
 
 			try {
-				var errorCmd = new SqlCommand ("SELECT * FROM test", Connection);
-				AssertEx.Exception<InvalidOperationException> (
-					() => errorCmd.ExecuteReaderAsync ().Wait (NetworkTimeout));
+				var errorCmd = CreateCommand ("SELECT * FROM test");
+				AssertEx.TaskFailed<InvalidOperationException> (
+					errorCmd.ExecuteNonQueryAsync (), NetworkTimeout);
 			} finally {
 				task.Result.Close ();
 			}
 		}
 
 		[Test]
+		[Category("Cancel")]
 		public void TestCommandTimeout ()
 		{
-			var cmd = new SqlCommand ("WAITFOR DELAY '00:00:15'", Connection);
-			cmd.CommandTimeout = (int)(NetworkTimeout.TotalMilliseconds + 999) / 1000;
-			var task = cmd.ExecuteNonQueryAsync ();
-			AssertEx.Exception<SqlException> (() => task.Wait (LongTimeout));
+			var cmd = CreateCommand ("WAITFOR DELAY '00:00:15'");
+			AssertEx.TaskFailed<SqlException> (
+				cmd.ExecuteNonQueryAsync (), LongTimeout);
 		}
 
 		[Test]
+		[Category("Sync")]
+		[Category("NotWorking")]
+		public void SyncTimeout ()
+		{
+			var cmd = new SqlCommand ("WAITFOR DELAY '00:00:30'", Connection);
+			cmd.CommandTimeout = CommandTimeout;
+			var start = DateTime.Now;
+
+			try {
+				cmd.ExecuteNonQuery ();
+				Assert.Fail ();
+			} catch (SqlException) {
+				Assert.That (DateTime.Now - start < TimeSpan.FromSeconds (2 * CommandTimeout));
+			}
+		}
+
+		[Test]
+		[Category("Sync")]
+		[Category("NotWorking")]
+		public void TraditionalTimeout ()
+		{
+			var cmd = new SqlCommand ("WAITFOR DELAY '00:00:30'", Connection);
+			cmd.CommandTimeout = CommandTimeout;
+			var start = DateTime.Now;
+
+			var ar = cmd.BeginExecuteNonQuery (_ => {
+				Console.WriteLine ("TRADITIONAL CALLBACK!");
+			}, null);
+
+			bool result = ar.AsyncWaitHandle.WaitOne (TimeSpan.FromSeconds (3 * CommandTimeout));
+			Console.WriteLine ("AFTER WAITING: {0} {1} {2}", DateTime.Now - start,
+			                   CommandTimeout, result);
+			Assert.IsFalse (result);
+			Assert.That (DateTime.Now - start > TimeSpan.FromSeconds (2 * CommandTimeout));
+		}
+
+		[Test]
+		[Category("Cancel")]
 		public void Cancel ()
 		{
-			var cmd = new SqlCommand ("WAITFOR DELAY '00:00:15'", Connection);
+			var cmd = new SqlCommand ("WAITFOR DELAY '00:00:30'", Connection);
 			var task = cmd.ExecuteNonQueryAsync ();
 			Task.Delay (NetworkTimeout).Wait ();
 			cmd.Cancel ();
-			AssertEx.Exception<SqlException> (() => task.Wait (LongTimeout));
+			AssertEx.TaskFailed<SqlException> (task, LongTimeout);
 		}
 
 		[Test]
+		[Category("Cancel")]
 		public void CancelWithToken ()
 		{
 			var cmd = new SqlCommand ("WAITFOR DELAY '00:00:15'", Connection);
 			var cts = new CancellationTokenSource (NetworkTimeout);
-			var task = cmd.ExecuteNonQueryAsync (cts.Token);
-			AssertEx.Exception<SqlException> (() => task.Wait (LongTimeout));
+			AssertEx.TaskFailed<SqlException> (
+				cmd.ExecuteNonQueryAsync (cts.Token), LongTimeout);
 		}
 
+		[Test]
+		[Category("Database")]
+		public void TestWait ()
+		{
+			var cmd = new SqlCommand ("WAITFOR DELAY '00:00:05'", Connection);
+			var task = cmd.ExecuteNonQueryAsync ();
+			Console.WriteLine ("TEST WAIT");
+			AssertEx.TaskCompleted (task, TimeSpan.FromSeconds (15));
+		}
+
+		[Test]
+		[Category("Sync")]
+		[Category("NotWorking")]
+		public void SyncTimeoutAndRead ()
+		{
+			var cmd = new SqlCommand ("WAITFOR DELAY '00:05:00'", Connection);
+			cmd.CommandTimeout = CommandTimeout;
+			var start = DateTime.Now;
+
+			try {
+				cmd.ExecuteNonQuery ();
+				Assert.Fail ();
+			} catch (SqlException) {
+				Assert.That (DateTime.Now - start < TimeSpan.FromSeconds (2 * CommandTimeout));
+			}
+
+			var select = CreateCommand ("SELECT * FROM test");
+			var reader = select.ExecuteReader ();
+			// var reader = AssertEx.TaskCompleted (select.ExecuteReaderAsync (), NetworkTimeout);
+			reader.Close ();
+		}
+
+		public static void DumpSqlException (SqlException ex)
+		{
+			Console.WriteLine ("SQL EXCEPTION: {0:x} {1:x} {2:x} {3:x} - {4}",
+			                   ex.Class, ex.ErrorCode, ex.Number,
+			                   ex.State, ex.Errors.Count);
+			foreach (var obj in ex.Errors) {
+				var error = (SqlError)obj;
+				Console.WriteLine ("  ERROR: {0} - {1:x} {2} {3} {4} {5} - |{6}|",
+				                   error, error.Class, error.Number, error.State,
+				                   error.Source, error.Server, error.Procedure);
+			}
+		}
+
+		[Test]
+		[Category("Cancel")]
+		public void CancelAndRead ()
+		{
+			Console.Error.WriteLine ("TEST CANCEL AND TEST");
+			var cmd = CreateCommand ("WAITFOR DELAY '00:05:00'");
+			var cts = new CancellationTokenSource (NetworkTimeout);
+			var task = cmd.ExecuteNonQueryAsync (cts.Token);
+			AssertEx.TaskFailed<SqlException> (task, LongTimeout);
+
+			var select = CreateCommand ("SELECT * FROM test");
+			var reader = AssertEx.TaskCompleted (select.ExecuteReaderAsync (), NetworkTimeout);
+			reader.Close ();
+		}
+
+		[Test]
+		[Category("Test")]
+		public void TestParams ()
+		{
+			var cmd = CreateCommand ("SELECT * FROM test WHERE a = @a");
+			var param = new SqlParameter ("a", SqlDbType.Int);
+			param.Value = 1;
+			cmd.Parameters.Add (param);
+
+			cmd.Prepare ();
+
+			var reader = AssertEx.TaskCompleted (cmd.ExecuteReaderAsync (), NetworkTimeout);
+			reader.Close ();
+		}
+
+		[Test]
+		[Category("Test")]
+		public void DelayProc ()
+		{
+			var cmd = CreateCommand ("Delay");
+			cmd.CommandType = CommandType.StoredProcedure;
+			cmd.CommandTimeout = 10;
+
+			var param = new SqlParameter ("@delay", SqlDbType.DateTime);
+			param.Value = DateTime.Parse ("00:00:01");
+			cmd.Parameters.Add (param);
+
+			AssertEx.TaskCompleted (cmd.ExecuteNonQueryAsync (), TimeSpan.FromSeconds (15));
+		}
 	}
 }
