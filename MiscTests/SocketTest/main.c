cd99eaef65898c93d110f2fdc62fff9daa57a8e6
Martin Baulig <martin.baulig@gmail.com>
2012-06-27 05:59:39 +0200

    Mac OS doesn't like interface = 0 here.

--- MiscTests/SocketTest/main.c
+++ MiscTests/SocketTest/main.c
@@ -4,25 +4,44 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
 
 int main (int argc, char *argv[])
 {
-	int sock = socket (PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	int sock = socket (PF_INET6, SOCK_DGRAM, 0);
 	printf ("SOCKET: %d\n", sock);
 
-	struct sockaddr_in saddr;
+	struct sockaddr_in6 saddr;
 	memset (&saddr, 0, sizeof (saddr));
-	saddr.sin_family = PF_INET;
-	saddr.sin_port = htons (1);
-	saddr.sin_addr.s_addr = inet_addr ("127.0.0.1");
+	saddr.sin6_family = PF_INET6;
+	saddr.sin6_port = htons (8888);
+	saddr.sin6_addr = in6addr_any;
 	
-	int ret = connect (sock, (struct sockaddr*)&saddr, sizeof (saddr));
-	printf ("CONNECT: %d - %s\n", ret, strerror (errno));
+	struct addrinfo* addr;
+	if (getaddrinfo ("ff02::1", NULL, NULL, &addr)) {
+		fprintf (stderr, "getaddrinfo(): %s\n", strerror (errno));
+		return -1;
+	}
 	
-	char buffer [1024];
-	ret = recv (sock, buffer, 1024, 0);
+	if (bind (sock, (const struct sockaddr*)&saddr, sizeof (saddr))) {
+		fprintf (stderr, "bind(): %s\n", strerror (errno));
+		return -1;
+	}
 	
-	printf ("RECV: %d\n", ret);
+	int iface = if_nametoindex ("lo0");
+	printf ("IFACE: %d\n", iface);
+	
+	struct ipv6_mreq mreq;
+	memset (&mreq, 0, sizeof (mreq));
+	memcpy (&mreq.ipv6mr_multiaddr, &((struct sockaddr_in6 *) addr->ai_addr)->sin6_addr, sizeof(mreq.ipv6mr_multiaddr));
+	mreq.ipv6mr_interface = 0;
+	
+	if (setsockopt (sock, IPPROTO_IPV6, IPV6_JOIN_GROUP, &mreq, sizeof(mreq))) {
+		fprintf (stderr, "setsockopt(): %s\n", strerror (errno));
+		return -1;
+	}
+	
+	printf ("OK\n");
 	
 	return 0;
 }
