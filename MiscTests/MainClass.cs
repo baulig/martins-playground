6c1eb511cb48c291e2362a4cb2230d5a22746d98
Martin Baulig <martin.baulig@gmail.com>
2012-06-28 20:53:06 +0200

    Done with the UdpSocketTests.

--- MiscTests/MainClass.cs
+++ MiscTests/MainClass.cs
@@ -10,20 +10,6 @@ namespace MiscTests
 	{
 		public static void Main (string[] args)
 		{
-			foreach (var addr in Dns.GetHostAddresses ("localhost")) {
-				Console.WriteLine ("LOCALHOST: {0}", addr);
-			}
-			Console.WriteLine ();
-
-			var entry = Dns.GetHostEntry (string.Empty);
-			Console.WriteLine ("HOST ENTRY: {0} {1}", entry, entry.HostName);
-			foreach (var addr in entry.AddressList)
-				Console.WriteLine ("HOST ADDR: {0}", addr);
-			foreach (var alias in entry.Aliases)
-				Console.WriteLine ("HOST ALIAS: {0}", alias);
-
-			var test = new UdpClientTest ();
-			test.JoinMulticastGroup1_IPv6 ();
 		}
 	}
 }
